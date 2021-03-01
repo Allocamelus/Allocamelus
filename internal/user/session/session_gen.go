2fb22c58e7cd69aca3a2d81e330ac9e38a49ebb6
JDinABox <37222721+JDinABox@users.noreply.github.com>
2021-03-01 04:38:51 -0500

    Add user private key decrypton & PrivateKey to session

--- internal/user/session/session_gen.go
+++ internal/user/session/session_gen.go
@@ -42,6 +42,12 @@ func (z *Session) DecodeMsg(dc *msgp.Reader) (err error) {
 				err = msgp.WrapError(err, "Perms")
 				return
 			}
+		case "privateKey":
+			z.PrivateKey, err = dc.ReadString()
+			if err != nil {
+				err = msgp.WrapError(err, "PrivateKey")
+				return
+			}
 		case "loginToken":
 			z.LoginToken, err = dc.ReadBytes(z.LoginToken)
 			if err != nil {
@@ -61,9 +67,23 @@ func (z *Session) DecodeMsg(dc *msgp.Reader) (err error) {
 
 // EncodeMsg implements msgp.Encodable
 func (z *Session) EncodeMsg(en *msgp.Writer) (err error) {
-	// map header, size 4
+	// omitempty: check for empty values
+	zb0001Len := uint32(5)
+	var zb0001Mask uint8 /* 5 bits */
+	if z.PrivateKey == "" {
+		zb0001Len--
+		zb0001Mask |= 0x8
+	}
+	// variable map header, size zb0001Len
+	err = en.Append(0x80 | uint8(zb0001Len))
+	if err != nil {
+		return
+	}
+	if zb0001Len == 0 {
+		return
+	}
 	// write "loggedIn"
-	err = en.Append(0x84, 0xa8, 0x6c, 0x6f, 0x67, 0x67, 0x65, 0x64, 0x49, 0x6e)
+	err = en.Append(0xa8, 0x6c, 0x6f, 0x67, 0x67, 0x65, 0x64, 0x49, 0x6e)
 	if err != nil {
 		return
 	}
@@ -92,6 +112,18 @@ func (z *Session) EncodeMsg(en *msgp.Writer) (err error) {
 		err = msgp.WrapError(err, "Perms")
 		return
 	}
+	if (zb0001Mask & 0x8) == 0 { // if not empty
+		// write "privateKey"
+		err = en.Append(0xaa, 0x70, 0x72, 0x69, 0x76, 0x61, 0x74, 0x65, 0x4b, 0x65, 0x79)
+		if err != nil {
+			return
+		}
+		err = en.WriteString(z.PrivateKey)
+		if err != nil {
+			err = msgp.WrapError(err, "PrivateKey")
+			return
+		}
+	}
 	// write "loginToken"
 	err = en.Append(0xaa, 0x6c, 0x6f, 0x67, 0x69, 0x6e, 0x54, 0x6f, 0x6b, 0x65, 0x6e)
 	if err != nil {
@@ -108,9 +140,20 @@ func (z *Session) EncodeMsg(en *msgp.Writer) (err error) {
 // MarshalMsg implements msgp.Marshaler
 func (z *Session) MarshalMsg(b []byte) (o []byte, err error) {
 	o = msgp.Require(b, z.Msgsize())
-	// map header, size 4
+	// omitempty: check for empty values
+	zb0001Len := uint32(5)
+	var zb0001Mask uint8 /* 5 bits */
+	if z.PrivateKey == "" {
+		zb0001Len--
+		zb0001Mask |= 0x8
+	}
+	// variable map header, size zb0001Len
+	o = append(o, 0x80|uint8(zb0001Len))
+	if zb0001Len == 0 {
+		return
+	}
 	// string "loggedIn"
-	o = append(o, 0x84, 0xa8, 0x6c, 0x6f, 0x67, 0x67, 0x65, 0x64, 0x49, 0x6e)
+	o = append(o, 0xa8, 0x6c, 0x6f, 0x67, 0x67, 0x65, 0x64, 0x49, 0x6e)
 	o = msgp.AppendBool(o, z.LoggedIn)
 	// string "userId"
 	o = append(o, 0xa6, 0x75, 0x73, 0x65, 0x72, 0x49, 0x64)
@@ -121,6 +164,11 @@ func (z *Session) MarshalMsg(b []byte) (o []byte, err error) {
 	if err != nil {
 		err = msgp.WrapError(err, "Perms")
 		return
+	}
+	if (zb0001Mask & 0x8) == 0 { // if not empty
+		// string "privateKey"
+		o = append(o, 0xaa, 0x70, 0x72, 0x69, 0x76, 0x61, 0x74, 0x65, 0x4b, 0x65, 0x79)
+		o = msgp.AppendString(o, z.PrivateKey)
 	}
 	// string "loginToken"
 	o = append(o, 0xaa, 0x6c, 0x6f, 0x67, 0x69, 0x6e, 0x54, 0x6f, 0x6b, 0x65, 0x6e)
@@ -164,6 +212,12 @@ func (z *Session) UnmarshalMsg(bts []byte) (o []byte, err error) {
 				err = msgp.WrapError(err, "Perms")
 				return
 			}
+		case "privateKey":
+			z.PrivateKey, bts, err = msgp.ReadStringBytes(bts)
+			if err != nil {
+				err = msgp.WrapError(err, "PrivateKey")
+				return
+			}
 		case "loginToken":
 			z.LoginToken, bts, err = msgp.ReadBytesBytes(bts, z.LoginToken)
 			if err != nil {
@@ -184,6 +238,6 @@ func (z *Session) UnmarshalMsg(bts []byte) (o []byte, err error) {
 
 // Msgsize returns an upper bound estimate of the number of bytes occupied by the serialized message
 func (z *Session) Msgsize() (s int) {
-	s = 1 + 9 + msgp.BoolSize + 7 + msgp.Int64Size + 6 + z.Perms.Msgsize() + 11 + msgp.BytesPrefixSize + len(z.LoginToken)
+	s = 1 + 9 + msgp.BoolSize + 7 + msgp.Int64Size + 6 + z.Perms.Msgsize() + 11 + msgp.StringPrefixSize + len(z.PrivateKey) + 11 + msgp.BytesPrefixSize + len(z.LoginToken)
 	return
 }
