b943c15e93cdf1ed3f1919dfea4a95382055eb35
JDinABox <37222721+JDinABox@users.noreply.github.com>
2021-03-07 15:46:40 -0500

    Add get user

--- internal/router/handlers/api/v1/user/get.go
+++ internal/router/handlers/api/v1/user/get.go
@@ -1,11 +1,37 @@
 package user
 
 import (
+	"database/sql"
+
+	"github.com/allocamelus/allocamelus/internal/router/handlers/api/apierr"
+	"github.com/allocamelus/allocamelus/internal/user"
 	"github.com/allocamelus/allocamelus/pkg/fiberutil"
+	"github.com/allocamelus/allocamelus/pkg/logger"
 	"github.com/gofiber/fiber/v2"
 )
 
+type getResponse struct {
+	user.User
+}
+
 // Get user handler
 func Get(c *fiber.Ctx) error {
-	return fiberutil.JSON(c, 200, fiber.Map{"type": "get"})
+	uniqueName := c.Params("uniqueName")
+	if len(uniqueName) == 0 {
+		return apierr.ErrNotFound(c)
+	}
+	userID, err := user.GetIDByUniqueName(uniqueName)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			logger.Error(err)
+			return apierr.ErrSomthingWentWrong(c)
+		}
+		return apierr.ErrNotFound(c)
+	}
+	u, err := user.GetPublic(userID)
+	if logger.Error(err) {
+		return apierr.ErrSomthingWentWrong(c)
+	}
+
+	return fiberutil.JSON(c, 200, getResponse{u})
 }
