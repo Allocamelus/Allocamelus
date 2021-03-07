b61966ddaa81f0b700c7d79e769cbe4ced01074b
JDinABox <37222721+JDinABox@users.noreply.github.com>
2021-03-07 00:02:28 -0500

    Move user keys to their own table & Update functions

--- internal/user/keys.go
+++ internal/user/keys.go
@@ -3,6 +3,8 @@ package user
 import (
 	"database/sql"
 	"errors"
+	"strings"
+	"time"
 
 	"github.com/allocamelus/allocamelus/internal/data"
 	"github.com/allocamelus/allocamelus/internal/g"
@@ -17,16 +19,39 @@ var (
 	// ErrGeneratingKeys Error for when pgp key generation or encryption fails
 	ErrGeneratingKeys = errors.New("user/user: Error generating/encrypting user keys")
 	// ErrDecryptingKey Error Decrypting Key
-	ErrDecryptingKey  = errors.New("user/user: Error Decrypting Key")
-	prePublicKey      *sql.Stmt
-	prePrivateKey     *sql.Stmt
-	prePrivateKeySalt *sql.Stmt
+	ErrDecryptingKey     = errors.New("user/user: Error Decrypting Key")
+	preKeyID             *sql.Stmt
+	prePublicKey         *sql.Stmt
+	prePrivateKey        *sql.Stmt
+	prePrivateKeySalt    *sql.Stmt
+	prePrivKeyAndSalt    *sql.Stmt
+	preBackupKey         *sql.Stmt
+	preUpdateKeyReplaced *sql.Stmt
+	preInsertKey         *sql.Stmt
 )
 
+func keySelectQueryBuilder(p data.Prepare, items ...string) *sql.Stmt {
+	var itemsPart strings.Builder
+	for i, v := range items {
+		if i > 0 {
+			itemsPart.WriteRune(',')
+		}
+		itemsPart.WriteString(" " + v)
+
+	}
+	return p(`SELECT` + itemsPart.String() + ` FROM UserKeys WHERE userId = ? ORDER BY userKeyId DESC LIMIT 1`)
+}
+
 func initKeys(p data.Prepare) {
-	prePublicKey = p(`SELECT publicKey FROM Users WHERE userId = ? LIMIT 1`)
-	prePrivateKey = p(`SELECT privateKey FROM Users WHERE userId = ? LIMIT 1`)
-	prePrivateKeySalt = p(`SELECT privateKeySalt FROM Users WHERE userId = ? LIMIT 1`)
+	preKeyID = keySelectQueryBuilder(p, "userKeyId")
+	prePublicKey = keySelectQueryBuilder(p, "publicKey")
+	prePrivateKey = keySelectQueryBuilder(p, "privateKey")
+	prePrivateKeySalt = keySelectQueryBuilder(p, "keySalt")
+	prePrivKeyAndSalt = keySelectQueryBuilder(p, "privateKey", "keySalt")
+	preBackupKey = keySelectQueryBuilder(p, "backupKey")
+	preUpdateKeyReplaced = p(`UPDATE UserKeys SET replaced = ? WHERE userKeyId = ?`)
+	preInsertKey = p(`INSERT INTO UserKeys (userId, created, publicKey, keySalt, privateKey, backupKey)
+		VALUES (?, ?, ?, ?, ?, ?)`)
 }
 
 // GenerateKeys from User
@@ -45,18 +70,66 @@ func (u *User) GenerateKeys(password string) error {
 	return nil
 }
 
+// UpdatePassword for User
+//
+// Password Reset Event should be called after this
+func UpdatePassword(userID int64, password string) (backupKey string, err error) {
+	var user User
+	user.ID = userID
+	// Used in key generation
+	user.UniqueName, err = GetUniqueNameByID(userID)
+	if err != nil {
+		return
+	}
+	// generate new keys
+	err = user.GenerateKeys(password)
+	if err != nil {
+		return
+	}
+	// update key
+	err = user.UpdateKey()
+	if err != nil {
+		return
+	}
+	return user.encodedBackupKey, nil
+}
+
+// UpdateKey for user
+func (u *User) UpdateKey() error {
+	keyID, err := GetKeyID(u.ID)
+	if err != nil {
+		return err
+	}
+	// Update old key's replaced time
+	_, err = preUpdateKeyReplaced.Exec(time.Now().Unix(), keyID)
+	if err != nil {
+		return err
+	}
+
+	return u.insertKey()
+}
+
+func (u *User) insertKey() error {
+	_, err := preInsertKey.Exec(
+		u.ID, time.Now().Unix(),
+		u.PublicKey, u.PrivateKeySalt,
+		u.PrivateKey, u.BackupKey,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // GetAndDecryptPK get and decrypt private key
 //	return string (private key) error (err)
 func GetAndDecryptPK(userID int64, password string) (*pgp.PrivateKey, error) {
 	pk := new(pgp.PrivateKey)
-	cryptPK, err := GetPrivateKey(userID)
+	cryptPK, cost, err := GetPrivKeyAndSalt(userID)
 	if err != nil {
 		return nil, err
 	}
-	cost, err := GetPrivateKeySalt(userID)
-	if err != nil {
-		return nil, err
-	}
+
 	costObj, _, err := argon2id.Parse(cost)
 	// Should not happen log just incase
 	if logger.Error(err) {
@@ -74,6 +147,12 @@ func GetAndDecryptPK(userID int64, password string) (*pgp.PrivateKey, error) {
 	return pk, nil
 }
 
+// GetKeyID get user's Key id
+func GetKeyID(userID int64) (keyID, err error) {
+	err = preKeyID.QueryRow(userID).Scan(&keyID)
+	return
+}
+
 // GetPublicKey get user's encrypted publicKey
 func GetPublicKey(userID int64) (pk pgp.PublicKey, err error) {
 	err = prePublicKey.QueryRow(userID).Scan(&pk.Armored)
@@ -89,6 +168,18 @@ func GetPrivateKey(userID int64) (pk string, err error) {
 // GetPrivateKeySalt get user's privateKeySalt
 func GetPrivateKeySalt(userID int64) (pkSalt string, err error) {
 	err = prePrivateKeySalt.QueryRow(userID).Scan(&pkSalt)
+	return
+}
+
+// GetPrivKeyAndSalt get user's privateKey and Salt
+func GetPrivKeyAndSalt(userID int64) (pk string, pkSalt string, err error) {
+	err = prePrivKeyAndSalt.QueryRow(userID).Scan(&pk, &pkSalt)
+	return
+}
+
+// GetBackupKey get user's encrypted BackupKey
+func GetBackupKey(userID int64) (bk string, err error) {
+	err = preBackupKey.QueryRow(userID).Scan(&bk)
 	return
 }
 
