921bd3873cfdff61168835b4082858cd4653cf3e
JDinABox <37222721+JDinABox@users.noreply.github.com>
2021-03-24 20:24:06 -0400

    Move to svg icons & update form text input

---
+++ web/app/src/components/form/SmallText.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-sm text-gray-700 dark:text-gray-400">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
