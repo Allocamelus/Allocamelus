5809a148497212946d3113c8ba9a408d9a094d38
jd <jd@angelstosaints.com>
2021-03-30 06:34:14 -0400

    Add router scrollBehavior

--- web/app/src/router.js
+++ web/app/src/router.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 const routes = [
   {
     path: "/about",
@@ -34,8 +35,16 @@ const routes = [
     props: true
   }
 ];
+
 export default createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  // Provide the history implementation to use. We are using the hash history for simplicity here.
   history: createWebHistory(),
   routes, // short for `routes: routes`
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
 })
