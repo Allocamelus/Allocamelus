b1e39f6e95318e79b57fbebf7c7895c8076334b5
jd <jd@angelstosaints.com>
2021-04-09 01:33:04 -0400

    Update avatar image functions

--- internal/user/avatar/imagemagick.go
+++ internal/user/avatar/imagemagick.go
@@ -2,10 +2,8 @@ package avatar
 
 import (
 	"errors"
-	"fmt"
 	"path/filepath"
 	"strconv"
-	"time"
 
 	"github.com/allocamelus/allocamelus/internal/g"
 	"github.com/allocamelus/allocamelus/pkg/logger"
@@ -13,67 +11,35 @@ import (
 	"gopkg.in/gographics/imagick.v3/imagick"
 )
 
-var MaxHightWidthStr = strconv.Itoa(int(MaxHightWidth))
+const (
+	WEBP = "WEBP"
+	PNG  = "PNG"
+	JPG  = "JPEG"
+	GIF  = "GIF"
+)
 
 func TransformAndSave(userId int64, tmpImagePath string) (newUrl string, err error) {
 	mw := imagick.NewMagickWand()
 	defer mw.Destroy()
-
-	start := time.Now()
-	slice := start
-
 	err = mw.ReadImage(tmpImagePath)
 	if err != nil {
 		return
 	}
-	fmt.Printf("Read: %v\n", time.Since(slice))
 
 	imgPath := "users/avatars/" + strconv.Itoa(int(userId)) + "/" + random.StringBase58(16)
 	fileImagePath := filepath.Join(g.Config.Path.Media, imgPath)
 
+	mw.StripImage()
+
 	// check for animation
 	if mw.GetNumberImages() > 1 {
-		slice = time.Now()
-		ret, err := imagick.ConvertImageCommand([]string{
-			"convert", tmpImagePath, "-coalesce", "-resize", MaxHightWidthStr + "x" + MaxHightWidthStr, "-layers", "Optimize", fileImagePath,
-		})
+		mw, err = transformAnimation(userId, mw)
 		if err != nil {
-			panic(err)
+			return
 		}
-		fmt.Printf("convert: %v\n", time.Since(slice))
-
-		fmt.Printf("Metadata:\n%s\n", ret.Meta)
-
-		slice = time.Now()
-		delay := mw.GetImageDelay()
-		aw := mw.CoalesceImages()
-		mw.Destroy()
-		defer aw.Destroy()
-
-		mw = imagick.NewMagickWand()
-		mw.SetImageDelay(delay)
-		// deferred ln:16
-		for i := 0; i < int(aw.GetNumberImages()); i++ {
-			aw.SetIteratorIndex(i)
-			img := aw.GetImage()
-
-			width, height, newHW := imgSquare(img)
-			cropAndResize(width, height, newHW, userId, img)
-			mw.AddImage(img)
-			img.Destroy()
-		}
-		fmt.Printf("convert: %v\n", time.Since(slice))
-
-		slice = time.Now()
-		//mw = mw.OptimizeImageLayers()
-		fmt.Printf("OptimizeImageLayers: %v\n", time.Since(slice))
-		slice = time.Now()
 		mw.WriteImages(fileImagePath, true)
-		fmt.Printf("Write: %v\n", time.Since(slice))
 	} else {
-		mw.StripImage()
-		width, height, newHW := imgSquare(mw)
-		err = cropAndResize(width, height, newHW, userId, mw)
+		err = transformImage(userId, mw)
 		if err != nil {
 			return
 		}
@@ -101,16 +67,54 @@ func imgSquare(mw *imagick.MagickWand) (width, height, newHW uint) {
 func cropAndResize(width, height, newHW uint, userId int64, mw *imagick.MagickWand) error {
 	err := mw.CropImage(newHW, newHW, int((width-newHW)/2), int((height-newHW)/2))
 	if err != nil {
-		// Log to catch abuse
-		logger.Error(errors.New("imagemagick: Error from User " + strconv.Itoa(int(userId)) + " | " + err.Error()))
+		if userId != 0 {
+			// Log to catch abuse
+			logger.Error(errors.New("imagemagick: Error from User " + strconv.Itoa(int(userId)) + " | " + err.Error()))
+		}
 		return err
 	}
 
-	err = mw.ResizeImage(MaxHightWidth, MaxHightWidth, imagick.FILTER_LANCZOS2)
-	if err != nil {
-		// Log to catch abuse
-		logger.Error(errors.New("imagemagick: Error from User " + strconv.Itoa(int(userId)) + " | " + err.Error()))
-		return err
+	// resize if bigger
+	if newHW > MaxHightWidth {
+		err = mw.ResizeImage(MaxHightWidth, MaxHightWidth, imagick.FILTER_LANCZOS2)
+		if err != nil {
+			if userId != 0 {
+				// Log to catch abuse
+				logger.Error(errors.New("imagemagick: Error from User " + strconv.Itoa(int(userId)) + " | " + err.Error()))
+			}
+			return err
+		}
 	}
+
 	return nil
+}
+
+func transformAnimation(userId int64, mw *imagick.MagickWand) (*imagick.MagickWand, error) {
+	delay := mw.GetImageDelay()
+	aw := mw.CoalesceImages()
+	mw.Destroy()
+	defer aw.Destroy()
+
+	nmw := imagick.NewMagickWand()
+	nmw.SetImageDelay(delay)
+	// deferred ln:16
+	for i := 0; i < int(aw.GetNumberImages()); i++ {
+		aw.SetIteratorIndex(i)
+		img := aw.GetImage()
+		err := transformImage(userId, img)
+		if err != nil {
+			return nmw, err
+		}
+		nmw.AddImage(img)
+		img.Destroy()
+	}
+	if nmw.GetImageFormat() == GIF {
+		nmw.OptimizeImageLayers()
+	}
+	return nmw, nil
+}
+
+func transformImage(userId int64, mw *imagick.MagickWand) error {
+	width, height, newHW := imgSquare(mw)
+	return cropAndResize(width, height, newHW, userId, mw)
 }
