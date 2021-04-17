b799533c17fbe7bbdc567a3f6e887ea3a6b1f681
JDinABox <37222721+JDinABox@users.noreply.github.com>
2021-04-16 23:58:04 -0400

    Add avatar deduplication and upload optimization

--- internal/user/avatar/cleanup.go
+++ internal/user/avatar/cleanup.go
@@ -10,58 +10,65 @@ import (
 
 var (
 	preDeactivateOld *sql.Stmt
-	preHasOld        *sql.Stmt
 	preGetOld        *sql.Stmt
 	preDelete        *sql.Stmt
 )
 
-func deactivateOld(userId, currentAvatarId int64) error {
+func deactivateOld(userId int64) error {
 	if preDeactivateOld == nil {
-		preDeactivateOld = g.Data.Prepare(`UPDATE UserAvatars SET active = 0 WHERE userAvatarId != ? AND userID = ? AND active = 1`)
+		preDeactivateOld = g.Data.Prepare(`UPDATE UserAvatars
+			SET active = 0
+			WHERE userAvatarId IN (
+				SELECT userAvatarId FROM (
+					SELECT userAvatarId FROM UserAvatars 
+					ORDER BY userAvatarId DESC
+					LIMIT 1, 18446744073709551615
+				) tmp
+			) AND userID = ? AND active = 1
+			`) // Deactivate all but the latest
 	}
-	_, err := preDeactivateOld.Exec(currentAvatarId, userId)
+	_, err := preDeactivateOld.Exec(userId)
 	return err
 }
 
 // CleanupOld removes deactivated avatars from file store by userId
 func CleanupOld(userId int64) error {
-	if preHasOld == nil {
-		preHasOld = g.Data.Prepare(`SELECT EXISTS(SELECT * FROM UserAvatars WHERE userId = ? AND active = 0)`)
+	if preGetOld == nil {
+		preGetOld = g.Data.Prepare(`SELECT UA.fileType, UA.hash
+			FROM UserAvatars UA
+			WHERE userId = ? AND active = 0
+				AND NOT EXISTS (
+					SELECT *
+					FROM UserAvatars
+					WHERE UA.hash = hash AND active = 1
+				)`)
 	}
-	var hasOld bool
-	err := preHasOld.QueryRow(userId).Scan(&hasOld)
+	rows, err := preGetOld.Query(userId)
 	if err != nil {
 		return err
 	}
-	if hasOld {
-		if preGetOld == nil {
-			preGetOld = g.Data.Prepare(`SELECT userAvatarId FROM UserAvatars WHERE userId = ? AND active = 0`)
-		}
-		rows, err := preGetOld.Query(userId)
+	defer rows.Close()
+
+	for rows.Next() {
+		var (
+			fileType fileutil.Format
+			b58hash  string
+		)
+		err := rows.Scan(&fileType, &b58hash)
 		if err != nil {
 			return err
 		}
-		defer rows.Close()
-
-		for rows.Next() {
-			var avatarId int64
-			err := rows.Scan(&avatarId)
-			if err != nil {
-				return err
-			}
-			remove(avatarId)
-		}
-
+		remove(b58hash, fileType)
 	}
 	return nil
 }
 
 // remove avatar file and db entry
-func remove(avatarId int64) error {
-	os.RemoveAll(fileutil.FilePath(selectorPath(avatarId, "")))
+func remove(b58hash string, fileType fileutil.Format) error {
+	os.RemoveAll(fileutil.FilePath(selectorPath(b58hash, fileType, true)))
 	if preDelete == nil {
-		preDelete = g.Data.Prepare(`DELETE FROM UserAvatars WHERE userAvatarId=?`)
+		preDelete = g.Data.Prepare(`DELETE FROM UserAvatars WHERE fileType=? AND hash=?`)
 	}
-	_, err := preDelete.Exec(avatarId)
+	_, err := preDelete.Exec(fileType, b58hash)
 	return err
 }
