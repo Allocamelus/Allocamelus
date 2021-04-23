29c20f064630919ac8f77068eea674fb555ce398
jd <jd@angelstosaints.com>
2021-04-23 02:44:44 -0400

    Add user canView function

---
+++ internal/user/view.go
@@ -0,0 +1,28 @@
+package user
+
+import "errors"
+
+var ErrViewMeNot = errors.New("user/view: Error can't view user")
+
+// CanView can userId be viewed by session user
+// 	return nil if userId can be viewed
+func CanView(userId int64, sUser *Session) error {
+	t, err := GetType(userId)
+	if err != nil {
+		return err
+	}
+	if t.Public() {
+		return nil
+	}
+	if !sUser.LoggedIn {
+		return ErrViewMeNot
+	}
+	following, err := Following(sUser.UserID, userId)
+	if err != nil {
+		return err
+	}
+	if !following {
+		return ErrViewMeNot
+	}
+	return nil
+}
