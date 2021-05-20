b8ee1df72f39fde003b16f4928f44f12214c2e21
jd <jd@angelstosaints.com>
2021-05-20 16:52:00 -0400

    Update site keys

--- web/app/src/api/meta/captcha/siteKeys.ts
+++ web/app/src/api/meta/captcha/siteKeys.ts
@@ -1,8 +1,42 @@
 import v1 from "../../v1";
-import { API_MetaCaptchaSiteKeys } from "../../../models/api_meta";
+export class CaptchaSiteKeys {
+  siteKeys: {
+    easy: string,
+    moderate: string,
+    hard: string,
+    all: string,
+  };
+  difficulties: {
+    user: {
+      create: string
+      emailToken: string
+    }
+  }
+
+  static createFrom(source: any = {}) { // skipcq: JS-0323, JS-0306
+    return new CaptchaSiteKeys(source);
+  }
+
+  constructor(source: any = {}) { // skipcq: JS-0323
+    if ('string' === typeof source) source = JSON.parse(source);
+    this.siteKeys = source["site-keys"]
+    this.difficulties = source["difficulties"]
+  }
+
+  siteKey(key: "easy" | "moderate" | "hard" | "all" | string) {
+    var siteKey: string
+    try {
+      siteKey = this.siteKeys[key]
+    } catch {
+      siteKey = this.siteKeys.all
+    }
+    return siteKey
+  }
+}
+
 
 export async function siteKeys() {
   return v1.get("meta/captcha/site-keys").then(r => {
-    return API_MetaCaptchaSiteKeys.createFrom(r.data)
+    return CaptchaSiteKeys.createFrom(r.data)
   })
 }
