441ea80a10d66b3cfb474216aa29fbd7a4170b42
jd <jd@angelstosaints.com>
2021-07-21 05:06:34 -0400

    Add paramsBool to fiberutil

--- pkg/fiberutil/params.go
+++ pkg/fiberutil/params.go
@@ -20,9 +20,32 @@ func ParamsInt64(c *fiber.Ctx, key string, defaultValue ...int64) int64 {
 	return int64(paramInt)
 }
 
+// ParamsBool get Bool parameters
+func ParamsBool(c *fiber.Ctx, key string, defaultValue ...bool) bool {
+	boolStr := c.Params(key)
+	if boolStr == "" {
+		return defBool(defaultValue)
+	}
+	switch boolStr {
+	case "true", "t", "yes", "y":
+		return true
+	case "false", "f", "no", "n":
+		return false
+	default:
+		return defBool(defaultValue)
+	}
+}
+
 func defInt64(defaultValue []int64) int64 {
 	if len(defaultValue) > 0 {
 		return defaultValue[0]
 	}
 	return 0
+}
+
+func defBool(defaultValue []bool) bool {
+	if len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+	return false
 }
