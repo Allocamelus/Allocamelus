936511156425e9d187a2b485bf090fc4d63def08
jd <jd@angelstosaints.com>
2021-08-16 13:04:20 -0400

    Update configs & Add commands to makefile

---
+++ vetur.config.js
@@ -0,0 +1,12 @@
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+  // **optional** default: `{}`
+  settings: {
+    "vetur.useWorkspaceDependencies": true,
+    "vetur.experimental.templateInterpolationService": true
+  },
+  // **optional** default: `[{ root: './' }]`
+  projects: [
+    './web/app', 
+  ]
+}
