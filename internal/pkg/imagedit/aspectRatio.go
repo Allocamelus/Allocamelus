48923bd4c8d5a4b7e114dfe04c91495f5b88875d
JDinABox <37222721+JDinABox@users.noreply.github.com>
2021-10-20 16:07:05 -0400

    Chore: Reduce code reuse

--- internal/pkg/imagedit/aspectRatio.go
+++ internal/pkg/imagedit/aspectRatio.go
@@ -36,24 +36,24 @@ func (img *Image) AR(ar AspectRatio) (width, height int) {
 
 func (img *Image) ARMaxSize(ar AspectRatio, maxPx int) (width, height int) {
 	width, height = img.AR(ar)
-	wf := float64(width)
-	hf := float64(height)
-	mpf := float64(maxPx)
 
-	if width > maxPx {
-		resizeFloat := mpf / wf
-		wf = resizeFloat * wf
-		hf = resizeFloat * hf
-	}
-
-	height = int(hf)
-	if height > maxPx {
-		resizeFloat := mpf / hf
-		hf = resizeFloat * hf
-		wf = resizeFloat * wf
-	}
+	maxPxF := float64(maxPx)
+	// Resize for width
+	wf, hf := resizeAr(float64(width), float64(height), maxPxF)
+	// resize for height
+	hf, wf = resizeAr(hf, wf, maxPxF)
 
 	width = int(wf)
 	height = int(hf)
 	return
+}
+
+// resizeAr reduces x to maxX if x > maxX
+func resizeAr(x, y, maxX float64) (float64, float64) {
+	if x > maxX {
+		resizeFloat := maxX / x
+		x = resizeFloat * x
+		y = resizeFloat * y
+	}
+	return x, y
 }
