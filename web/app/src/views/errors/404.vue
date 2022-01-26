2a3d17b6b9c0642772f03ba63f6f98b6f11a03be
JDinABox <37222721+JDinABox@users.noreply.github.com>
2022-01-26 00:59:04 -0500

    Update 404 page

--- web/app/src/views/errors/404.vue
+++ web/app/src/views/errors/404.vue
@@ -7,12 +7,17 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
 import Box from "../../components/box/Box.vue";
 import { Html404Func } from "../../components/htmlErrors";
 
 export default defineComponent({
+  setup() {
+    document.title = `Error 404 (Not Found) - ${
+      import.meta.env.VITE_SITE_NAME
+    }`;
+  },
   computed: {
     errorHtml() {
       return Html404Func(this.$route.fullPath);
