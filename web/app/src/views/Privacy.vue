83c0e2d6f7d2e51515c1553a93f5de1b4eff1636
JDinABox <37222721+JDinABox@users.noreply.github.com>
2022-04-16 06:41:05 -0400

    Add templates

--- web/app/src/views/Privacy.vue
+++ web/app/src/views/Privacy.vue
@@ -2,6 +2,242 @@
   <div class="container flex flex-col py-5">
     <Box class="flex flex-col gap-6 rounded-xl p-5 text-base">
       <h1 :class="textStyle.h2">Privacy Policy</h1>
+      <div>
+        <h2 id="collect" :class="textStyle.h3" class="mb-3">
+          <a href="#collect">What information do we collect?</a>
+        </h2>
+        <ul class="flex list-outside list-disc flex-col gap-1 pl-5">
+          <li>
+            Basic account information: If you register on this server, you may
+            be asked to enter a username, an e-mail address and a password. You
+            may also enter additional profile information such as a display name
+            and biography, and upload a profile picture. The username, display
+            name, biography, profile picture are always listed publicly.
+          </li>
+          <li>
+            Posts, following and other public information: The list of people
+            you follow is listed publicly, the same is true for your followers.
+            When you submit a message, the date and time is stored as well as
+            the application you submitted the message from. Messages may contain
+            media attachments, such as pictures and videos. Public and unlisted
+            posts are available publicly. When you feature a post on your
+            profile, that is also publicly available information. Your posts are
+            delivered to your followers, in some cases it means they are
+            delivered to different servers and copies are stored there. When you
+            delete posts, this is likewise delivered to your followers. The
+            action of reblogging or favouriting another post is always public.
+          </li>
+          <li>
+            Direct and followers-only posts: All posts are stored and processed
+            on the server. Followers-only posts are delivered to your followers
+            and users who are mentioned in them, and direct posts are delivered
+            only to users mentioned in them. In some cases it means they are
+            delivered to different servers and copies are stored there. We make
+            a good faith effort to limit the access to those posts only to
+            authorized persons, but other servers may fail to do so. Therefore
+            it's important to review servers your followers belong to. You may
+            toggle an option to approve and reject new followers manually in the
+            settings. Please keep in mind that the operators of the server and
+            any receiving server may view such messages, and that recipients may
+            screenshot, copy or otherwise re-share them. Do not share any
+            dangerous information over {{ siteName }}.
+          </li>
+          <li>
+            IPs and other metadata: When you log in, we record the IP address
+            you log in from, as well as the name of your browser application.
+            All the logged in sessions are available for your review and
+            revocation in the settings. The latest IP address used is stored for
+            up to 12 months. We also may retain server logs which include the IP
+            address of every request to our server.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2 id="use" :class="textStyle.h3" class="mb-3">
+          <a href="#use">What do we use your information for?</a>
+        </h2>
+        Any of the information we collect from you may be used in one of the
+        following ways:
+        <ul class="flex list-outside list-disc flex-col gap-1 pl-5">
+          <li>
+            To personalize your experience &mdash; your information helps us to
+            better respond to your individual needs.
+          </li>
+          <li>
+            To improve our site &mdash; we continually strive to improve our
+            site offerings based on the information and feedback we receive from
+            you.
+          </li>
+          <li>
+            To improve customer service &mdash; your information helps us to
+            more effectively respond to your customer service requests and
+            support needs.
+          </li>
+          <li>
+            To send periodic emails &mdash; The email address you provide may be
+            used to send you information, notifications that you request about
+            changes to topics or in response to your user name, respond to
+            inquiries, and/or other requests or questions.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2 id="protect" :class="textStyle.h3" class="mb-3">
+          <a href="#protect">How do we protect your information?</a>
+        </h2>
+        We implement a variety of security measures to maintain the safety of
+        your personal information when you enter, submit, or access your
+        personal information.
+      </div>
+
+      <div>
+        <h2 id="data-retention" :class="textStyle.h3" class="mb-3">
+          <a href="#data-retention">What is your data retention policy?</a>
+        </h2>
+        We will make a good faith effort to: * Retain server logs containing the
+        IP address of all requests to this server no more than 90 days. * Retain
+        the IP addresses associated with registered users and their posts no
+        more than 5 years.
+      </div>
+
+      <div>
+        <h2 id="cookies" :class="textStyle.h3" class="mb-3">
+          <a href="#cookies">Do we use cookies?</a>
+        </h2>
+        Yes. Cookies are small files that a site or its service provider
+        transfers to your computer's hard drive through your Web browser (if you
+        allow). These cookies enable the site to recognize your browser and, if
+        you have a registered account, associate it with your registered
+        account. We use cookies to understand and save your preferences for
+        future visits and compile aggregate data about site traffic and site
+        interaction so that we can offer better site experiences and tools in
+        the future. We may contract with third-party service providers to assist
+        us in better understanding our site visitors. These service providers
+        are not permitted to use the information collected on our behalf except
+        to help us conduct and improve our business.
+      </div>
+
+      <div>
+        <h2 id="disclose" :class="textStyle.h3" class="mb-3">
+          <a href="#disclose"
+            >Do we disclose any information to outside parties?</a
+          >
+        </h2>
+        <div class="flex flex-col gap-2">
+          <div>
+            We do not sell, trade, or otherwise transfer to outside parties your
+            personally identifiable information. This does not include trusted
+            third parties who assist us in operating our site, conducting our
+            business, or servicing you, so long as those parties agree to keep
+            this information confidential. We may also release your information
+            when we believe release is appropriate to comply with the law,
+            enforce our site policies, or protect ours or others rights,
+            property, or safety.
+          </div>
+          <div>
+            If this server is in the USA: Our site, products and services are
+            all directed to people who are at least 13 years old. If you are
+            under the age of 13, per the requirements of COPPA (<to-link
+              to="https://en.wikipedia.org/wiki/Children%27s_Online_Privacy_Protection_Act"
+              class="link"
+              >Children's Online Privacy Protection Act</to-link
+            >) do not use this site.
+          </div>
+        </div>
+        We do not sell, trade, or otherwise transfer to outside parties your
+        personally identifiable information. This does not include trusted third
+        parties who assist us in operating our site, conducting our business, or
+        servicing you, so long as those parties agree to keep this information
+        confidential. We may also release your information when we believe
+        release is appropriate to comply with the law, enforce our site
+        policies, or protect ours or others rights, property, or safety.
+        However, non-personally identifiable visitor information may be provided
+        to other parties for marketing, advertising, or other uses.
+      </div>
+
+      <div>
+        <h2 id="third-party" :class="textStyle.h3" class="mb-3">
+          <a href="#third-party">Third party links</a>
+        </h2>
+        Occasionally, at our discretion, we may include or offer third party
+        products or services on our site. These third party sites have separate
+        and independent privacy policies. We therefore have no responsibility or
+        liability for the content and activities of these linked sites.
+        Nonetheless, we seek to protect the integrity of our site and welcome
+        any feedback about these sites.
+      </div>
+
+      <div>
+        <h2 id="coppa" :class="textStyle.h3" class="mb-3">
+          <a href="#coppa">Site usage by children</a>
+        </h2>
+        <div class="flex flex-col gap-2">
+          <div>
+            If this server is in the EU or the EEA: Our site, products and
+            services are all directed to people who are at least 16 years old.
+            If you are under the age of 16, per the requirements of the GDPR
+            (<to-link
+              to="https://en.wikipedia.org/wiki/General_Data_Protection_Regulation"
+              class="link"
+              >General Data Protection Regulation</to-link
+            >) do not use this site.
+          </div>
+          <div>
+            If this server is in the USA: Our site, products and services are
+            all directed to people who are at least 13 years old. If you are
+            under the age of 13, per the requirements of COPPA (<to-link
+              to="https://en.wikipedia.org/wiki/Children%27s_Online_Privacy_Protection_Act"
+              class="link"
+              >Children's Online Privacy Protection Act</to-link
+            >) do not use this site.
+          </div>
+          <div>
+            Law requirements can be different if this server is in another
+            jurisdiction.
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h2 id="online" :class="textStyle.h3" class="mb-3">
+          <a href="#online">Online Privacy Policy Only</a>
+        </h2>
+        This online privacy policy applies only to information collected through
+        our site and not to information collected offline.
+      </div>
+
+      <div>
+        <h2 id="consent" :class="textStyle.h3" class="mb-3">
+          <a href="#consent">Your Consent</a>
+        </h2>
+        By using our site, you consent to our web site privacy policy.
+      </div>
+
+      <div>
+        <h2 id="changes" :class="textStyle.h3" class="mb-3">
+          <a href="#changes">Changes to our Privacy Policy</a>
+        </h2>
+        <div class="flex flex-col gap-2">
+          <div>
+            If we decide to change our privacy policy, we will post those
+            changes on this page.
+          </div>
+          <div>
+            This document is CC-BY-SA. It was last updated Apr 16, 2022.
+          </div>
+          <div>
+            Originally adapted from the
+            <to-link to="https://github.com/mastodon/mastodon" class="link">
+              Mastodon privacy policy</to-link
+            >, adapted from the
+            <to-link to="https://github.com/discourse/discourse" class="link">
+              Discourse privacy policy</to-link
+            >.
+          </div>
+        </div>
+      </div>
     </Box>
   </div>
 </template>
@@ -18,6 +254,7 @@ export default defineComponent({
     document.title = `Privacy Policy - ${import.meta.env.VITE_SITE_NAME}`;
     return {
       ...toRefs(data),
+      siteName: import.meta.env.VITE_SITE_NAME,
     };
   },
   components: { Box, ToLink },
