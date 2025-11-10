66fe9c5383421cc68bb2e07126dedcb91411af5f
JD <37222721+JDinABox@users.noreply.github.com>
2025-11-10 01:05:24 -0500

    Feature/terms (#19)
    * Add tos & update privacy policy

--- web/app/src/views/PagePrivacy.vue
+++ web/app/src/views/PagePrivacy.vue
@@ -1,7 +1,253 @@
 <template>
   <div class="container flex flex-col py-5">
     <Box class="flex flex-col gap-6 rounded-xl p-5 text-base">
-      <h1 class="h1">Privacy Policy</h1>
+      <h1 class="h2">Privacy Policy</h1>
+      <div>
+        <h2 id="collect" class="h3 mb-3">
+          <a href="#collect">What information do we collect?</a>
+        </h2>
+        <ul class="flex list-outside list-disc flex-col gap-1 pl-5">
+          <li>
+            Basic account information: If you register on this server, you may
+            be asked to enter a username, an e-mail address and a password. You
+            may also enter additional profile information such as a display name
+            and biography, and upload a profile picture. The username, display
+            name, biography, profile picture are always listed publicly.
+          </li>
+          <li>
+            Posts, following and other public information: The list of people
+            you follow is listed publicly, the same is true for your followers.
+            When you submit a message, the date and time is stored as well as
+            the application you submitted the message from. Messages may contain
+            media attachments, such as pictures and videos. Public and unlisted
+            posts are available publicly. When you feature a post on your
+            profile, that is also publicly available information. Your posts are
+            delivered to your followers, in some cases it means they are
+            delivered to different servers and copies are stored there. When you
+            delete posts, this is likewise delivered to your followers. The
+            action of reblogging or favouriting another post is always public.
+          </li>
+          <li>
+            Direct and followers-only posts: All posts are stored and processed
+            on the server. Followers-only posts are delivered to your followers
+            and users who are mentioned in them, and direct posts are delivered
+            only to users mentioned in them. In some cases it means they are
+            delivered to different servers and copies are stored there. We make
+            a good faith effort to limit the access to those posts only to
+            authorized persons, but other servers may fail to do so. Therefore
+            it's important to review servers your followers belong to. You may
+            toggle an option to approve and reject new followers manually in the
+            settings. Please keep in mind that the operators of the server and
+            any receiving server may view such messages, and that recipients may
+            screenshot, copy or otherwise re-share them. Do not share any
+            dangerous information over {{ siteName }}.
+          </li>
+          <li>
+            IPs and other metadata: When you log in, we record the IP address
+            you log in from, as well as the name of your browser application.
+            All the logged in sessions are available for your review and
+            revocation in the settings. The latest IP address used is stored for
+            up to 12 months. We also may retain server logs which include the IP
+            address of every request to our server.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2 id="use" class="h3 mb-3">
+          <a href="#use">What do we use your information for?</a>
+        </h2>
+        Any of the information we collect from you may be used in one of the
+        following ways:
+        <ul class="flex list-outside list-disc flex-col gap-1 pl-5">
+          <li>
+            To provide and operate the Service, including displaying your public
+            profile, posts, and social interactions.
+          </li>
+          <li>
+            To secure your account and prevent fraud by using your login IP
+            address.
+          </li>
+          <li>
+            To communicate with you about service updates, policy changes, and
+            to respond to your inquiries.
+          </li>
+          <li>
+            To improve our site &mdash; we continually strive to improve our
+            site offerings based on the information and feedback we receive from
+            you.
+          </li>
+          <li>
+            To send periodic emails &mdash; The email address you provide may be
+            used to send you information, notifications that you request about
+            changes to topics or in response to your user name, respond to
+            inquiries, and/or other requests or questions.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2 id="data-retention" class="h3 mb-3">
+          <a href="#data-retention">What is your data retention policy?</a>
+        </h2>
+        <ul class="flex list-outside list-disc flex-col gap-1 pl-5">
+          <li>
+            Server logs containing the IP address of all requests to this server
+            are retained for no more than 90 days.
+          </li>
+          <li>
+            IP addresses associated with registered users are retained for a
+            period of 12 months to ensure account security and investigate
+            potential misuse of the platform.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2 id="rights" class="h3 mb-3">
+          <a href="#rights">Your Data Rights and Choices</a>
+        </h2>
+        <div class="flex flex-col gap-2">
+          <div>
+            You have the right to access, correct, or delete your personal data.
+            You can exercise these rights through your account settings or by
+            contacting us at {{ contactEmail }}.
+          </div>
+          <ul class="flex list-outside list-disc flex-col gap-1 pl-5">
+            <li>
+              <b>Right to Access:</b> You can request a copy of your personal
+              data.
+            </li>
+            <li>
+              <b>Right to Correction:</b> You can modify your account
+              information at any time through your account settings.
+            </li>
+            <li>
+              <b>Right to Deletion:</b> You can delete your account, which will
+              remove your personal data from our systems.
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <h2 id="protect" class="h3 mb-3">
+          <a href="#protect">How do we protect your information?</a>
+        </h2>
+        We use a variety of security measures to protect your information,
+        including encryption of data in transit (HTTPS), password hashing, and
+        regular security reviews of our infrastructure.
+      </div>
+
+      <div>
+        <h2 id="cookies" class="h3 mb-3">
+          <a href="#cookies">Do we use cookies?</a>
+        </h2>
+        Yes. Cookies are small files that a site or its service provider
+        transfers to your computer's hard drive through your Web browser (if you
+        allow). These cookies enable the site to recognize your browser and, if
+        you have a registered account, associate it with your registered
+        account. We use cookies to understand and save your preferences for
+        future visits and compile aggregate data about site traffic and site
+        interaction so that we can offer better site experiences and tools in
+        the future.
+      </div>
+
+      <div>
+        <h2 id="disclose" class="h3 mb-3">
+          <a href="#disclose"
+            >Do we disclose any information to outside parties?</a
+          >
+        </h2>
+        <div class="flex flex-col gap-2">
+          <div>
+            We do not sell, trade, or otherwise transfer to outside parties your
+            personally identifiable information. This does not include trusted
+            third parties who assist us in operating our site, conducting our
+            business, or servicing you, so long as those parties agree to keep
+            this information confidential. We may also release your information
+            when we believe release is appropriate to comply with the law,
+            enforce our site policies, or protect ours or others rights,
+            property, or safety.
+          </div>
+          <div>
+            For sending emails, we use
+            <to-link to="https://www.mailgun.com/" class="link">Mailgun</to-link
+            >. You can review their privacy policy for more information on how
+            they handle data.
+          </div>
+          <div>
+            However, non-personally identifiable visitor information may be
+            provided to other parties for marketing, advertising, or other uses.
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h2 id="third-party" class="h3 mb-3">
+          <a href="#third-party">Third party links</a>
+        </h2>
+        Occasionally, at our discretion, we may include or offer third party
+        products or services on our site. These third party sites have separate
+        and independent privacy policies. We therefore have no responsibility or
+        liability for the content and activities of these linked sites.
+        Nonetheless, we seek to protect the integrity of our site and welcome
+        any feedback about these sites.
+      </div>
+
+      <div>
+        <h2 id="coppa" class="h3 mb-3">
+          <a href="#coppa">Site usage by children</a>
+        </h2>
+        <div class="flex flex-col gap-2">
+          <div>
+            Our service is not directed to individuals under the age of 16 in
+            the EU/EEA or 13 in the USA. We do not knowingly collect personal
+            information from children. If you are a parent or guardian and
+            believe your child has provided us with personal information, please
+            contact us at {{ contactEmail }} so we can take appropriate action.
+          </div>
+          <div>
+            Law requirements can be different if this server is in another
+            jurisdiction.
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h2 id="online" class="h3 mb-3">
+          <a href="#online">Online Privacy Policy Only</a>
+        </h2>
+        This online privacy policy applies only to information collected through
+        our site and not to information collected offline.
+      </div>
+
+      <div>
+        <h2 id="consent" class="h3 mb-3">
+          <a href="#consent">Your Consent</a>
+        </h2>
+        By using our site, you consent to our web site privacy policy.
+      </div>
+
+      <div>
+        <h2 id="contact" class="h3 mb-3">
+          <a href="#contact">Contacting Us</a>
+        </h2>
+        If there are any questions regarding this privacy policy you may contact
+        us at {{ contactEmail }}.
+      </div>
+
+      <div>
+        <h2 id="changes" class="h3 mb-3">
+          <a href="#changes">Changes to our Privacy Policy</a>
+        </h2>
+        <div class="flex flex-col gap-2">
+          <div>
+            If we decide to change our privacy policy, we will post those
+            changes on this page.
+          </div>
+        </div>
+      </div>
     </Box>
   </div>
 </template>
@@ -17,6 +263,8 @@ export default defineComponent({
     document.title = `Privacy Policy - ${import.meta.env.VITE_SITE_NAME}`;
     return {
       ...toRefs(data),
+      siteName: import.meta.env.VITE_SITE_NAME,
+      contactEmail: import.meta.env.VITE_CONTACT_EMAIL,
     };
   },
   components: { Box },
--- web/app/src/views/PageTerms.vue
+++ web/app/src/views/PageTerms.vue
@@ -1,18 +1,149 @@
 <template>
-  <div class="container flex flex-col py-5">Terms</div>
+  <div class="container flex flex-col py-5">
+    <Box class="flex flex-col gap-6 rounded-xl p-5 text-base">
+      <h1 class="h2">Terms of Service</h1>
+      <p>Last Updated: November 9th, 2025</p>
+
+      <div>
+        <p>
+          Welcome to Allocamelus! These Terms of Service ("Terms") govern your
+          use of the Allocamelus website and services (collectively, the
+          "Service"), so please read them carefully.
+        </p>
+      </div>
+
+      <div>
+        <h2 id="acceptance-of-terms" class="h3 mb-3">
+          <a href="#acceptance-of-terms">Acceptance of Terms</a>
+        </h2>
+        <p>
+          By using the Service, you agree to be bound by these Terms. If you do
+          not agree to these Terms, you may not use the Service.
+        </p>
+      </div>
+
+      <div>
+        <h2 id="description-of-service" class="h3 mb-3">
+          <a href="#description-of-service">Description of Service</a>
+        </h2>
+        <p>
+          Allocamelus provides a platform for users to post text and images. We
+          may update, modify, or discontinue any part of the Service at any
+          time.
+        </p>
+      </div>
+
+      <div>
+        <h2 id="user-accounts" class="h3 mb-3">
+          <a href="#user-accounts">User Accounts</a>
+        </h2>
+        <p>
+          You are responsible for maintaining the security of your account and
+          for all activities that occur under your account. You must immediately
+          notify us of any unauthorized use of your account.
+        </p>
+      </div>
+
+      <div>
+        <h2 id="user-content" class="h3 mb-3">
+          <a href="#user-content">User Content</a>
+        </h2>
+        <p>
+          You are solely responsible for the content you post ("User Content").
+          By posting User Content, you grant us a worldwide, non-exclusive,
+          royalty-free license to use, reproduce, modify, and distribute your
+          User Content in connection with the Service and for our marketing and
+          promotional purposes.
+        </p>
+      </div>
+
+      <div>
+        <h2 id="prohibited-activities" class="h3 mb-3">
+          <a href="#prohibited-activities">Prohibited Activities</a>
+        </h2>
+        <p>
+          You agree not to post any User Content that is illegal or unlawful.
+        </p>
+      </div>
+
+      <div>
+        <h2 id="termination" class="h3 mb-3">
+          <a href="#termination">Termination</a>
+        </h2>
+        <p>
+          We may terminate or suspend your account at any time, for any reason,
+          without notice.
+        </p>
+      </div>
+
+      <div>
+        <h2 id="disclaimer-of-warranties" class="h3 mb-3">
+          <a href="#disclaimer-of-warranties">Disclaimer of Warranties</a>
+        </h2>
+        <p>
+          The Service is provided "as is," without any warranties of any kind.
+          We do not warrant that the Service will be error-free or
+          uninterrupted.
+        </p>
+      </div>
+
+      <div>
+        <h2 id="limitation-of-liability" class="h3 mb-3">
+          <a href="#limitation-of-liability">Limitation of Liability</a>
+        </h2>
+        <p>
+          We will not be liable for any damages arising from your use of the
+          Service.
+        </p>
+      </div>
+
+      <div>
+        <h2 id="governing-law" class="h3 mb-3">
+          <a href="#governing-law">Governing Law</a>
+        </h2>
+        <p>
+          These Terms are governed by the laws of the Commonwealth of Kentucky,
+          without regard to its conflict of law provisions.
+        </p>
+      </div>
+
+      <div>
+        <h2 id="changes-to-terms" class="h3 mb-3">
+          <a href="#changes-to-terms">Changes to Terms</a>
+        </h2>
+        <p>
+          We may modify these Terms at any time. We will notify you of any
+          changes by posting the new Terms on this page.
+        </p>
+      </div>
+
+      <div>
+        <h2 id="contact-us" class="h3 mb-3">
+          <a href="#contact-us">Contact Us</a>
+        </h2>
+        <p>
+          If you have any questions about these Terms, please contact us at
+          {{ contactEmail }}.
+        </p>
+      </div>
+    </Box>
+  </div>
 </template>
 
 <script>
 import { defineComponent, toRefs, reactive } from "vue";
+import Box from "@/components/box/Box.vue";
 export default defineComponent({
   setup() {
     const data = reactive({});
 
-    document.title = `Terms - ${import.meta.env.VITE_SITE_NAME}`;
+    document.title = `Terms of Service - ${import.meta.env.VITE_SITE_NAME}`;
 
     return {
       ...toRefs(data),
+      contactEmail: import.meta.env.VITE_CONTACT_EMAIL,
     };
   },
+  components: { Box },
 });
 </script>
